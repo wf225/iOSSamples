02ea77eae506fdb5e8d88da71c29c9a7a8d8f172
Bill Wu <bill.wu@autodesk.com>
2016-03-08 16:58:10 +0800

    Add iOS_CI.

---
+++ iOS_CI/iOS_CI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_CI
+//
+//  Created by Bill Wu on 16/3/8.
+//  Copyright © 2016年 Autodesk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
