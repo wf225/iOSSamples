b2f22a36237d1b1a9d3971ac9877490273dd5cae
Bill Wu <bill.wu@autodesk.com>
2016-03-10 11:05:07 +0800

    Rename test cases.

--- iOS_CI_FreeAccount/iOS_CI_FreeAccountTests/iOS_CI_FreeAccountTests.swift
+++ iOS_CI_FreeAccount/iOS_CI_FreeAccountTests/iOS_CI_FreeAccountTests.swift
@@ -21,16 +21,20 @@ class iOS_CI_FreeAccountTests: XCTestCase {
         super.tearDown()
     }
     
-    func unitTestExample() {
+    func testExample1() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func unitTestPerformanceExample() {
+    func testPerformanceExample1() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testTrue() {
+        XCTAssert(true, "Can not be zero");
     }
     
 }
